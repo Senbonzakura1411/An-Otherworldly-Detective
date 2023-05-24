af9cee68ae28e83640cace3393a6be089606c678
Óscar Cruz <oscarcruz1411@gmail.com>
2023-05-24 00:09:21 -0600

    Project Setup

---
+++ Source/PhoenixCross/PhoenixCrossGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhoenixCrossGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHOENIXCROSS_API APhoenixCrossGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
